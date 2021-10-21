91df99da55f05a369974ac5f55a3a430e70501af
cmlegault <chris.legault@noaa.gov>
2021-10-21 13:13:27 -0400

    change master branch to main

--- test.R
+++ test.R
@@ -2,3 +2,12 @@
 test <- 1:10
 test <- test * 4
 test
+
+# 5 steps to convert master branch to main branch
+# https://stevenmortimer.com/5-steps-to-change-github-default-branch-from-master-to-main/
+git branch -m master main
+git push -u origin main
+git symbolic-ref refs/remotes/origin/HEAD refs/remotes/origin/main
+# change the default branch on GitHub to main
+# https://docs.github.com/en/github/administering-a-repository/setting-the-default-branch
+git push origin --delete master
